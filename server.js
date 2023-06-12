3707bfb7a9936d6bd34690e13e0b52f2cc414c67
Bijay Kadariya <bijaykadariya92@gmail.com>
2023-06-11 20:09:28 -0400

    Visualizing data as Json

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Import and Setup Dependencies
 
 const express = require("express")
+const drinks =require("./models/drinks")
+
 
 const app = express()
 const port = 3000
@@ -8,6 +10,10 @@ const port = 3000
 // Routes
 app.get("/", (req, res)=>{
     res.send("welcome to the gitpub app")
+})
+
+app.get("/drinks", (req, res)=>{
+    res.send(drinks)
 })
 
 // Listener
